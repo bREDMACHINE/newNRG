33e9e03b54da8014a7b309b84716801db8ef5e53
bREDMACHINE <yasenya1985@yandex.ru>
2022-12-13 22:21:41 +0300

    Initial commit

---
+++ src/main/java/get/a/big/head/newNRG/NewNrgApplication.java
@@ -0,0 +1,13 @@
+package get.a.big.head.newNRG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewNrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewNrgApplication.class, args);
+	}
+
+}
