b1589c127f97c16db6e5168fa0cf10253de8beb3
bREDMACHINE <yasenya1985@yandex.ru>
2023-04-11 23:57:13 +0300

    refactor - настройка открытия файлов на стороне клиента

--- client/src/main/java/get/a/big/head/newNRG/files/FileMapper.java
+++ client/src/main/java/get/a/big/head/newNRG/files/FileMapper.java
@@ -20,45 +20,45 @@ import java.util.*;
 public class FileMapper {
 
     public static DataFile toDataFile(Object object) {
-//        Reader reader = new StringReader(object.toString());
-//        JsonElement jsonElement = JsonParser.parseReader(reader);
-//        JsonObject jsonObject = jsonElement.getAsJsonObject();
-//        Long fileId = jsonObject.get("fileId").getAsLong();
-//        String name = jsonObject.get("name").getAsString();
-//        String contentType = jsonObject.get("contentType").getAsString();
-//        Boolean isEmpty = jsonObject.get("isEmpty").getAsBoolean();
-//        Long size = jsonObject.get("size").getAsLong();
-//        String type = contentType.replace("&", "/");
-//        if (!jsonObject.get("content").isJsonNull()) {
-//            System.out.println(jsonObject.get("content"));
-//            byte[] content = DatatypeConverter.parseBase64Binary(jsonObject.get("content").getAsString());
-//
-//            return DataFile.builder()
-//                    .fileId(fileId)
-//                    .name(name)
-//                    .contentType(type)
-//                    .isEmpty(isEmpty)
-//                    .size(size)
-//                    .content(content)
-//                    .build();
-//        }
-//        return DataFile.builder()
-//                .fileId(fileId)
-//                .name(name)
-//                .contentType(type)
-//                .isEmpty(isEmpty)
-//                .size(size)
-//                .build();
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(byte[].class, new ByteArrayAdapter())
-                .create();
-//        try {
-            JsonReader jsonReader = new JsonReader(new StringReader(object.toString()));
-//            jsonReader.close();
-            return gson.fromJson(jsonReader, DataFile.class);
-//        } catch (IOException e) {
-//            throw new NotFoundException("Файл не загружен");
-//        }
+        Reader reader = new StringReader(object.toString());
+        JsonElement jsonElement = JsonParser.parseReader(reader);
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        Long fileId = jsonObject.get("fileId").getAsLong();
+        String name = jsonObject.get("name").getAsString();
+        String contentType = jsonObject.get("contentType").getAsString();
+        Boolean isEmpty = jsonObject.get("isEmpty").getAsBoolean();
+        Long size = jsonObject.get("size").getAsLong();
+        String type = contentType.replace("&", "/");
+        if (!jsonObject.get("content").isJsonNull()) {
+            System.out.println(jsonObject.get("content"));
+            byte[] content = DatatypeConverter.parseBase64Binary(jsonObject.get("content").getAsString());
+
+            return DataFile.builder()
+                    .fileId(fileId)
+                    .name(name)
+                    .contentType(type)
+                    .isEmpty(isEmpty)
+                    .size(size)
+                    .content(content)
+                    .build();
+        }
+        return DataFile.builder()
+                .fileId(fileId)
+                .name(name)
+                .contentType(type)
+                .isEmpty(isEmpty)
+                .size(size)
+                .build();
+//        Gson gson = new GsonBuilder()
+//                .registerTypeAdapter(byte[].class, new ByteArrayAdapter())
+//                .create();
+////        try {
+//            JsonReader jsonReader = new JsonReader(new StringReader(object.toString()));
+////            jsonReader.close();
+//            return gson.fromJson(jsonReader, DataFile.class);
+////        } catch (IOException e) {
+////            throw new NotFoundException("Файл не загружен");
+////        }
     }
 
     public static DataFile toDataFile(File file) {
@@ -67,7 +67,6 @@ public class FileMapper {
         try {
             contentType = Files.probeContentType(file.toPath());
             bytes = Files.readAllBytes(file.toPath());
-            System.out.println(Arrays.toString(bytes));
         } catch (IOException e) {
             throw new RuntimeException("Файл не загружен", e);
         }
@@ -82,47 +81,48 @@ public class FileMapper {
 
     }
 
-    static class ByteArrayAdapter extends TypeAdapter<byte[]> {
-
-        @Override
-        public void write(final JsonWriter jsonWriter, final byte[] bytes) throws IOException {
-            new JsonPrimitive(Base64.getEncoder().encodeToString(bytes));
-        }
-
-        @Override
-        public byte[] read(final JsonReader reader) {
-            try {
-                StringBuilder builder = new StringBuilder();
-                switch (reader.peek()) {
-                    case BEGIN_ARRAY:
-                        reader.beginArray();
-                        break;
-                    case STRING:
-                        while (reader.hasNext()) {
-                            builder.append(reader.nextString());
-                        }
-                        break;
-                    case NUMBER:
-                        while (reader.hasNext()) {
-                            builder.append(reader.nextLong());
-                        }
-                        break;
-                    case END_ARRAY:
-                        reader.endArray();
-                        break;
-                }
-                return DatatypeConverter.parseBase64Binary(builder.toString());
-            } catch (IOException e) {
-                throw new NotFoundException("N");
-            }
-        }
-
-        public byte[] append(byte[] a1, byte[] a2) {
-            byte[] a1a2 = Arrays.copyOf(a1, a1.length + a2.length);
-            for (int i = a1.length; i < a1a2.length; i++) {
-                a1a2[i] = a2[i - a1.length];
-            }
-            return a1a2;
-        }
-    }
+//    static class ByteArrayAdapter extends TypeAdapter<byte[]> {
+//
+//        @Override
+//        public void write(final JsonWriter jsonWriter, final byte[] bytes) throws IOException {
+//            new JsonPrimitive(Base64.getEncoder().encodeToString(bytes));
+//        }
+//
+//        @Override
+//        public byte[] read(final JsonReader reader) {
+//            reader.setLenient(true);
+//            try {
+////                StringBuilder builder = new StringBuilder();
+////                while (reader.hasNext()) {
+//                    switch (reader.peek()) {
+//
+//                        case BEGIN_ARRAY:
+//                            reader.beginArray();
+//                            break;
+//                        case STRING:
+//                            String s = reader.toString();
+//                            System.out.println(s);
+//                            return DatatypeConverter.parseBase64Binary(s);
+//
+//
+//                        case END_ARRAY:
+//                            reader.endArray();
+//                            break;
+//                    }
+////                }
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//
+//        public byte[] append(byte[] a1, byte[] a2) {
+//            byte[] a1a2 = Arrays.copyOf(a1, a1.length + a2.length);
+//            for (int i = a1.length; i < a1a2.length; i++) {
+//                a1a2[i] = a2[i - a1.length];
+//            }
+//            return a1a2;
+//        }
+//    }
 }
